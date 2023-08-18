aaf3e55749d9c00b15a3f76f46da707fbf4df115
Kaitong Lu <lu.kai@northeastern.edu>
2023-08-18 15:14:46 -0700

    project

---
+++ src/business/Role/ClinicRole.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.Role;
+
+import business.Business;
+import business.Organization.ClinicOrganization;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import ui.Clinic.ClinicWorkAreaJPanel;
+
+/**
+ *
+ * @author raunak
+ */
+public class ClinicRole extends Role {
+
+    public ClinicRole() {
+        this.type = RoleType.ClinicRole;
+    }
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Business business) {
+        this.type = RoleType.ClinicRole;
+        return new ClinicWorkAreaJPanel(userProcessContainer, account, (ClinicOrganization) organization, business);
+    }
+
+}
